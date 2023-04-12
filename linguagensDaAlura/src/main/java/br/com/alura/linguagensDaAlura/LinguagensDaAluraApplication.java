536d254593152aa61a61b75906ab9129898de9e3
KUHAKU-17 <pablolira17@hotmail.com>
2023-04-12 17:48:11 -0300

    commit inicial

---
+++ linguagensDaAlura/src/main/java/br/com/alura/linguagensDaAlura/LinguagensDaAluraApplication.java
@@ -0,0 +1,14 @@
+package br.com.alura.linguagensDaAlura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinguagensDaAluraApplication {
+	// faltando opção de executar o codigo
+	public static void main(String[] args) {
+		SpringApplication.run(LinguagensDaAluraApplication.class, args);
+	}
+
+}
+
---
+++ linguagensDaAlura/src/test/java/br/com/alura/linguagensDaAlura/LinguagensDaAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.linguagensDaAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinguagensDaAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
